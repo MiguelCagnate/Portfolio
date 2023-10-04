14234d0d8a7533e360961371836ce8b303080367
omikeflies <omikeflies@gmail.com>
2023-10-04 18:01:05 +0200

    Main text and icons done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
-import whitebg from '../assets/whitebg.jpg'
+import React from "react";
+import whitebg from "../assets/whitebg.jpg";
+import { TypeAnimation } from "react-type-animation";
+import {
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
 
 const Main = () => {
   return (
     <div id="main">
-          <img className="w-full h-screen object-cover object-left scale-x-[-1]" src={whitebg} alt='white background' />
-          <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-              <h1>I am Miguel Cañate</h1>
+      <img
+        className="w-full h-screen object-cover scale-x-[-1]"
+        src={whitebg}
+        alt="white background"
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/70">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            I'm Miguel Cañate
+          </h1>
+          <h2 className="flex sm:text 3xl text-2xl pt-4 text-gray-800">
+            I'm a
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Developer",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Coder",
+                2000,
+                "Tech Enthusiast ",
+                2000,
+              ]}
+              wrapper="span"
+              cursor={true}
+              speed={30}
+              style={{ fontSize: "1em", paddingLeft: "5px" }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaTwitter className="cursor-pointer" size={20} />
+            <FaFacebook className="cursor-pointer" size={20} />
+            <FaInstagram className="cursor-pointer" size={20} />
+            <FaLinkedinIn className="cursor-pointer" size={20} />
           </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Main
+export default Main;
