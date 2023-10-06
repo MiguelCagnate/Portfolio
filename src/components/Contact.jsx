e6653f2b38646b0b80c0a549c8098b2e150f0969
omikeflies <omikeflies@gmail.com>
2023-10-06 13:04:24 +0200

    Contact form done and run

---
+++ src/components/Contact.jsx
@@ -0,0 +1,63 @@
+
+const Contact = () => {
+  return (
+    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
+        Contact
+      </h1>
+      <form
+        action="https://getform.io/f/93f54817-2c94-46a5-9796-e67f4e9e1cfb"
+        method="POST"
+        encType="multipart/form-data"
+      >
+        <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+          <div className="flex flex-col py-2">
+            <label>Name</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="text"
+              name="name"
+            ></input>
+          </div>
+          <div className="flex flex-col py-2">
+            <label className="uppercase text-sm py-2">Phone Number</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="text"
+              name="phone"
+            ></input>
+          </div>
+          <div className="flex flex-col py-2">
+            <label className="uppercase text-sm py-2">Email</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="email"
+              name="email"
+            ></input>
+          </div>
+          <div className="flex flex-col py-2">
+            <label className="uppercase text-sm py-2">Subject</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="text"
+              name="subject"
+            ></input>
+          </div>
+          <div className="flex flex-col py-2">
+            <label className="uppercase text-sm py-2">Message</label>
+            <textarea
+              className="border-2 rounded-lg p-3 border-gray-300"
+              rows="10"
+              name="message"
+            ></textarea>
+          </div>
+        </div>
+        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact
